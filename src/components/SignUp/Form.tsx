405f84a5d3174e08a16f8f3654805c8fa2beda73
DiegosebasRR <86979824+DiegosebasRR@users.noreply.github.com>
2023-06-19 18:24:38 -0500

    Sign Up

---
+++ src/components/SignUp/Form.tsx
@@ -0,0 +1,34 @@
+import Button from "../global/Button/Button";
+import Input from "../global/Imput/Input";
+import styles from "./Form.module.css";
+const Form = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerForm}>
+        <h1 className={styles.title}>Sign Un</h1>
+        <p className={styles.description}>
+          It's not long before you embark on this journey!
+        </p>
+        <form className={styles.form}>
+          <div className={styles.containerInputs}>
+            <Input label="Firstname" placeholder="firstname" />
+            <Input label="Lastname" placeholder="lastname" />
+          </div>
+          <Input label="E-mail" placeholder="name@email.com" />
+          <Input
+            label="Password"
+            placeholder="8+ Characters, 1 Capital letter"
+          />
+
+          <Button />
+        </form>
+        <h2 className={styles.text}>
+          You do not have an account?{" "}
+          <span className={styles.span}>Sign up</span>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/SignUp/SignUp.tsx
@@ -0,0 +1,13 @@
+import RightPartSign from "../global/RightPartSign/RightPartSign";
+import Form from "./Form";
+import styles from "./SignUp.module.css";
+const SignUp = () => {
+  return (
+    <div className={styles.container}>
+      <Form />
+      <RightPartSign />
+    </div>
+  );
+};
+
+export default SignUp;
